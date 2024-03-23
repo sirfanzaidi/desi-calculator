647f4cbc943f9e9a2df085adf926e0e170b03013
Irfan Hussain <I.hussain@expressmedia.int>
2024-03-23 15:16:06 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import Choices from "inquirer/lib/objects/choices.js";
+const answer = await inquirer.prompt([
+{message:"Enter first Digit", type: "number", name: "firstDigit"},
+{message:"Enter Second Digit", type: "number", name: "secondDigit"},
+{message:"select one of the operation to perform action", 
+type: "list",
+name:"operator",
+choices: ["addition", "substraction", "multiplication", "division"],},
+]);
+
+if (answer.operator === "addition")
+{console.log (answer.firstDigit + answer.secondDigit);}
+
+else if (answer.operator === "substraction")
+{console.log (answer.firstDigit - answer.secondDigit);}
+
+else if (answer.operator === "multiplication")
+{console.log (answer.firstDigit * answer.secondDigit);}
+
+else if (answer.operator === "division")
+{console.log (answer.firstDigit / answer.secondDigit);}
+else {console.log("please select valid operators")}
